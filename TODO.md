85805529c18ce303c4f1bb0e7d0b6430dcee62b7
Andre Almada <andre@asalmada.dev>
2025-10-13 21:19:06 -0300

    feat(critterpedia): changed db to postgres

--- TODO.md
+++ TODO.md
@@ -1,8 +1,4 @@
 - Critters
 	- Entender o spawn rates para incluir.
-	- ActionStorage para as imagens.
-	- Admin scope (namespace)
-	- Change DB to PSQL
 	- Deploy
 	- TurboFrame para availabilities dinâmico.
-	- Compartilhar codigo entre admin e non-admin (pasta shared)
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_10_12_030122) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "critter_availabilities", force: :cascade do |t|
     t.integer "critter_id", null: false
     t.string "hemisphere", null: false
